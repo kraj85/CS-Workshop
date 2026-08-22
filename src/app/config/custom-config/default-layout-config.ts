6f719dd75ef88283d0b855360adf94e7d9a8a7e6
Kumar Rituraj <kumar.rituraj@sap.com>
2026-08-22 21:10:12 +0530

    Breakpoint config

--- src/app/config/custom-config/default-layout-config.ts
+++ src/app/config/custom-config/default-layout-config.ts
@@ -1,18 +1,25 @@
-import { LayoutConfig } from "@spartacus/storefront";
+import { LayoutConfig } from '@spartacus/storefront';
 
-export const defaultLayoutConfig:LayoutConfig = {
-          layoutSlots: {
-        LandingPage2Template: {
-          pageFold: 'Section3',          // optional: change pageFold too
-          slots: [
-            'Section1',
-            'Section3',     // moved Section3 before Section2A
-            'Section2A',
-            'Section2B',
-            'Section2C',
-            'Section4',
-            'Section5',
-          ],
-        },
+export const defaultLayoutConfig: LayoutConfig = {
+  layoutSlots: {
+    LandingPage2Template: {
+      pageFold: 'Section3', // optional: change pageFold too
+      lg: {
+        slots: [
+          'Section1',
+          'Section3', // moved Section3 before Section2A
+          'Section2A',
+          'Section2B',
+          'Section2C',
+          'Section4',
+          'Section5',
+        ],
       },
-}
+      slots:[
+          'Section1',
+          'Section3',
+          'Section4',
+      ]
+    },
+  },
+};
