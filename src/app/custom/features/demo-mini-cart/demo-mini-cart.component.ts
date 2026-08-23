d6ca8d846984203aab5d42cab93b53989a751017
Kumar Rituraj <kumar.rituraj@sap.com>
2026-08-23 20:33:17 +0530

    remove 0 flicker

--- src/app/custom/features/demo-mini-cart/demo-mini-cart.component.ts
+++ src/app/custom/features/demo-mini-cart/demo-mini-cart.component.ts
@@ -2,11 +2,10 @@ import { Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ActiveCartFacade } from '@spartacus/cart/base/root';
-import { I18nModule, UrlModule } from '@spartacus/core';
-import { IconModule, ICON_TYPE } from '@spartacus/storefront';
-import { map, startWith, switchMap } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
 import { MiniCartComponent } from '@spartacus/cart/base/components/mini-cart';
+import { I18nModule, UrlModule } from '@spartacus/core';
+import { IconModule } from '@spartacus/storefront';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-demo-mini-cart',
@@ -18,8 +17,7 @@ export class DemoMiniCart extends MiniCartComponent{
   activeCartFacade = inject(ActiveCartFacade);
 
   entriesCount$ = this.activeCartFacade.getActive().pipe(
+    filter(cart => !!cart.code),
     map(cart => cart.entries?.length ?? 0)
   );
-
-
 }
